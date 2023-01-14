a80691365ad52a895281736e4d9d46d566cab3e2
vignesh-nayak <vigneshnayak555@gmail.com>
2023-01-14 12:23:48 +0530

    added alert in main

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style/style.css';
 
 const Main = () => {
+
+    useEffect(() => {
+        alert('Currently page is not responsive. specially in mobile/small screen, please enable desktop site for now. Will be fix it soon. Thank you.')
+    }, [])
+
     return (
         <div className='homeButton'>
             <a href="/login" className='link main-link'>Login</a>
